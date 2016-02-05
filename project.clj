a51f10dacc6e3006a3224646f53df62897157301
Brett Weaver <brett_weaver@intuit.com>
2016-02-05 13:30:14 -0800

    updating to latest version of tentacles

--- project.clj
+++ project.clj
@@ -5,4 +5,4 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [tentacles "0.3.0"]])
+                 [tentacles "0.5.1"]])
