82d3bc0d754db0b73f15d29ef626a3ecba07d758
Brett W <brett_weaver@intuit.com>
2016-06-28 12:00:12 -0700

    bug fix

--- src/hubub/core.clj
+++ src/hubub/core.clj
@@ -76,7 +76,7 @@
   (doseq [repo-name (github/list-repos org)]
     (doseq [access valid-access]
       (let [team-name (str repo-name "-" access)]
-        (set-users org input repo-name team-name valid-user-fn access)))))
+        (set-users org input repo-name team-name valid-user-fn)))))
 
 (defn- process
   [org input token valid-user-fn]
